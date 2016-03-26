adeb5bbd2e178ca8fdac7ce566b691a8ec67083d
Tobias Welther <tobiyasw@gmail.com>
2016-03-26 18:13:50 +0100

    Add Gyro Sensor.

---
+++ AndroidCodeSnippets/app/src/test/java/de/tu_darmstadt/smastra/sensors/AndroidGyroscopeSensorTest.java
@@ -0,0 +1,20 @@
+package de.tu_darmstadt.smastra.sensors;
+
+import android.hardware.Sensor;
+
+/**
+ * Created by Tobias Welther on 26.03.2016
+ */
+public class AndroidGyroscopeSensorTest extends Abstract3dAndroidSensorTest {
+
+
+    public AndroidGyroscopeSensorTest() {
+        super(Sensor.TYPE_GYROSCOPE);
+    }
+
+
+    @Override
+    protected Abstract3dAndroidSensor generateSut() {
+        return new AndroidGyroscopeSensor(context, 9_001);
+    }
+}
