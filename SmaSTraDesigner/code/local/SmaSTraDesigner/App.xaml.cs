1025a87a4bcbc0e7eea9d04b4ff45dc43a35b3ea
Tobias Welther <tobiyasw@gmail.com>
2016-09-25 14:46:16 +0200

    Update Workspace-Change. It now works, except for Tree/GUI-Clearing.

--- SmaSTraDesigner/code/local/SmaSTraDesigner/App.xaml.cs
+++ SmaSTraDesigner/code/local/SmaSTraDesigner/App.xaml.cs
@@ -6,6 +6,8 @@
 
     using BusinessLogic;
     using BusinessLogic.config;
+    using System.IO;
+    using BusinessLogic.utils;
 
     /// <summary>
     /// Interaction logic for App.xaml
@@ -28,15 +30,31 @@
         /// Switches the Workspace.
         /// </summary>
         /// <param name="newWorkspace"></param>
-        public void SwitchWorkspace(string newWorkspace)
+        public static void SwitchWorkspace(string newWorkspace)
         {
+            //Be sure the new Workspace exists:
+            if(!string.IsNullOrWhiteSpace(newWorkspace)) Directory.CreateDirectory(newWorkspace);
+
+            //Set the new Workspace:
             SmaSTraConfiguration.WORK_SPACE = newWorkspace;
 
+            //Copy all the Basic stuff in:
+            string newGeneratedPath = Path.Combine(SmaSTraConfiguration.WORK_SPACE, "generated");
+            if (  !Directory.Exists(newGeneratedPath) 
+                || Directory.EnumerateFileSystemEntries(newGeneratedPath).Empty())
+            {
+                string orgGeneratedPath = "generated";
+                DirCopy.PlainCopy(orgGeneratedPath, newGeneratedPath);
+            }
+
+
+            //Clear the current Tree and the GUI:
+            //TODO add reload of Transformation Tree here!
+
+            //Reload the managers:
             Singleton<SmaSTraConfiguration>.Instance.Reload();
             Singleton<ClassManager>.Instance.Reload();
             Singleton<Library>.Instance.loadLibrary();
-
-            //TODO add reload of Transformation Tree here!
         }
 
 		#endregion event handlers
