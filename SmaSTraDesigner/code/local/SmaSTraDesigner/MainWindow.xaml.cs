22d455af7b6d040249e340ad8a4b240d451a4b3d
Gerhard Säckel <gerhard.saeckel@yahoo.de>
2016-10-04 17:50:45 +0200

    + Switched Highlight colors + Element type buttons are now toggles again + fixed OutputHandle position bug

--- SmaSTraDesigner/code/local/SmaSTraDesigner/MainWindow.xaml.cs
+++ SmaSTraDesigner/code/local/SmaSTraDesigner/MainWindow.xaml.cs
@@ -54,34 +54,16 @@
 
         private void togDataSources_Checked(object sender, RoutedEventArgs e)
         {
-            int oldPosition = spnNodeTypeMenu.Children.IndexOf(gridSideMenu);
-            spnNodeTypeMenu.Children.RemoveAt(oldPosition);
-            int newPosition = spnNodeTypeMenu.Children.IndexOf(placeholderDataSources);
-            spnNodeTypeMenu.Children.Insert(newPosition, gridSideMenu);
-            ToggleButton_Checked(sender, e);
-
             Singleton<ClassManager>.Instance.SelectedCategory = "datasource";
         }
 
         private void togConversions_Checked(object sender, RoutedEventArgs e)
         {
-            int oldPosition = spnNodeTypeMenu.Children.IndexOf(gridSideMenu);
-            spnNodeTypeMenu.Children.RemoveAt(oldPosition);
-            int newPosition = spnNodeTypeMenu.Children.IndexOf(placeholderConversions);
-            spnNodeTypeMenu.Children.Insert(newPosition, gridSideMenu);
-            ToggleButton_Checked(sender, e);
-
             Singleton<ClassManager>.Instance.SelectedCategory = "conversion";
         }
 
         private void togTransformations_Checked(object sender, RoutedEventArgs e)
         {
-            int oldPosition = spnNodeTypeMenu.Children.IndexOf(gridSideMenu);
-            spnNodeTypeMenu.Children.RemoveAt(oldPosition);
-            int newPosition = spnNodeTypeMenu.Children.IndexOf(placeholderTransformations);
-            spnNodeTypeMenu.Children.Insert(newPosition, gridSideMenu);
-            ToggleButton_Checked(sender, e);
-
             Singleton<ClassManager>.Instance.SelectedCategory = "transformation";
         }
 
