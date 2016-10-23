882e023125dc3a1d30b74c851c2ea97f35669613
Tobias Welther <tobiyasw@gmail.com>
2016-10-23 18:44:30 +0200

    Added some header fields to the exceptions: from: time stamp from when to get, to: time stamp to where to get limit: limits the amount of results

--- SmaSTraWebServer/src/de/tu_darmstadt/smastra/rest/GetException.java
+++ SmaSTraWebServer/src/de/tu_darmstadt/smastra/rest/GetException.java
@@ -38,13 +38,34 @@ public class GetException extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	//Read from / to from Headers:
+    	String fromName = req.getHeader("from");
+    	String toName = req.getHeader("to");
+    	String limitName = req.getHeader("limit");
+
+    	long from = 0;
+    	long to = Long.MAX_VALUE;
+    	long limit = 10;
+
+    	//parse from / to:
+    	if(fromName != null) try{ from = Long.parseLong(fromName); }catch(Throwable exp){}
+    	if(toName != null) try{ to = Long.parseLong(toName); }catch(Throwable exp){}
+    	if(limitName != null) try{ limit = Long.parseLong(limitName); }catch(Throwable exp){}
+
+    	
     	File[] files = basePath.listFiles();
     	JsonArray root = new JsonArray();
     	if(files != null && files.length > 0){
     		for(File file : files){
+    			//Check if the limit is reached:
+    			if(limit -- <= 0) break;
+    			
+    			long time = Long.parseLong(file.getName().replace("Error-", "").replaceAll(".exception", ""));
+    			if(time < from || time > to) continue;
+    			
     			JsonObject obj = new JsonObject();
     			obj.addProperty("content", readFile(file));
-    			obj.addProperty("time", file.getName().replace("Error-", "").replaceAll(".exception", ""));
+    			obj.addProperty("time", time);
     			
     			root.add(obj);
     		}
