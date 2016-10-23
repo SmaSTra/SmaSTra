9be5563b478c9004ea62d94b83d711e8ed31e23c
Tobias Welther <tobiyasw@gmail.com>
2016-10-23 18:28:57 +0200

    Added Reading of Exceptions in Json format for the Server. (Not sure where to use though :D)

---
+++ SmaSTraWebServer/src/de/tu_darmstadt/smastra/rest/GetException.java
@@ -0,0 +1,63 @@
+package de.tu_darmstadt.smastra.rest;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+
+/**
+ * Servlet implementation class SmaSTra
+ */
+@WebServlet("/exceptions")
+public class GetException extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * The Base-Folder for the exceptions.
+	 */
+	private final File basePath = new File("/home/SmaSTra/Exceptions");
+
+    /**
+     * Default constructor. 
+     */
+    public GetException() {
+    	basePath.mkdirs();
+    }
+    
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	File[] files = basePath.listFiles();
+    	JsonArray root = new JsonArray();
+    	if(files != null && files.length > 0){
+    		for(File file : files){
+    			JsonObject obj = new JsonObject();
+    			obj.addProperty("content", readFile(file));
+    			obj.addProperty("time", file.getName().replace("Error-", "").replaceAll(".exception", ""));
+    			
+    			root.add(obj);
+    		}
+    	}
+    	
+    	resp.setStatus(200);
+    	resp.getWriter().write(root.toString());
+    }
+    
+    
+	private static String readFile(File path) throws IOException {
+		byte[] encoded = Files.readAllBytes(path.toPath());
+		return new String(encoded, Charset.defaultCharset());
+	}    
+
+}
